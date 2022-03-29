2b1ccc0f8c0f3850ae49e09f08869850c56a4471
Brendan Duncan <brendanduncan@gmail.com>
2022-03-29 10:53:51 -0700

    simplify tests; add shader test

--- loki/gl/shapes/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./gl_circle_mesh.js";
-export * from "./gl_cube_mesh.js";
-export * from "./gl_fullscreen_mesh.js";
-export * from "./gl_plane_mesh.js";
-export * from "./gl_sphere_mesh.js";
